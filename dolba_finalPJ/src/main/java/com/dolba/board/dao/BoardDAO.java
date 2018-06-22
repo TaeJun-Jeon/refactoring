dfc601eb727b05c8fbd14ba1eff2df055e940d2b
Xiyeon <Xiyeon@192.168.0.137>
2018-06-22 12:23:36 +0900

    q&a mapper, dao, dto 작성 -시연

--- dolba_finalPJ/src/main/java/com/dolba/board/dao/BoardDAO.java
+++ dolba_finalPJ/src/main/java/com/dolba/board/dao/BoardDAO.java
@@ -1,5 +1,12 @@
 package com.dolba.board.dao;
 
-public interface BoardDAO {
+import java.util.List;
 
+import com.dolba.dto.BoardDTO;
+
+public interface BoardDAO {
+	/**
+	 * qa게시판 모든 리스트 검색하는 메소드
+	 **/
+	public List<BoardDTO> selectAllQa();
 }
--- dolba_finalPJ/src/main/java/com/dolba/dto/BoardDTO.java
+++ dolba_finalPJ/src/main/java/com/dolba/dto/BoardDTO.java
@@ -1,5 +1,56 @@
 package com.dolba.dto;
 
 public class BoardDTO {
-
+	private String QAId;
+	private String ownerId;
+	private String QATitle;
+	private String QAContent;
+	private String QAWriteday;
+	private int QAReadnum;
+	private String QAPwd;
+	
+	public String getQAId() {
+		return QAId;
+	}
+	public void setQAId(String qAId) {
+		QAId = qAId;
+	}
+	public String getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(String ownerId) {
+		this.ownerId = ownerId;
+	}
+	public String getQATitle() {
+		return QATitle;
+	}
+	public void setQATitle(String qATitle) {
+		QATitle = qATitle;
+	}
+	public String getQAContent() {
+		return QAContent;
+	}
+	public void setQAContent(String qAContent) {
+		QAContent = qAContent;
+	}
+	public String getQAWriteday() {
+		return QAWriteday;
+	}
+	public void setQAWriteday(String qAWriteday) {
+		QAWriteday = qAWriteday;
+	}
+	public int getQAReadnum() {
+		return QAReadnum;
+	}
+	public void setQAReadnum(int qAReadnum) {
+		QAReadnum = qAReadnum;
+	}
+	public String getQAPwd() {
+		return QAPwd;
+	}
+	public void setQAPwd(String qAPwd) {
+		QAPwd = qAPwd;
+	}
+	
+	
 }
